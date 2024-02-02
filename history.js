f1910c8b0457bdde5f3d6d25021a4c3c790e6c90
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-02-02 15:42:07 +0530

    History of js

---
+++ history.js
@@ -0,0 +1,56 @@
+console.log("-------------------------------- 1995 ---------------------------------------");
+
+// Netscape Navigator (Browser) popular it think to make persnol programming language to make interact in websites.
+// Brendan Eich in that company who make js in 10 days
+// they firstly name it to MOKA
+// after they give name to Livescript
+// it cant make it popular as thry think
+// that time more popular language is java
+// due to low popularity they change name from Livescript to JavaScript.
+
+console.log("-------------------------------- 1996 ---------------------------------------");
+// Internet explorer thinks netscape navigator was very popular due to JS
+// thats why they copy some feature from js some added and named as JScript
+
+// due to 2 language more confusion was create
+
+
+// thats why they go to ecma 
+// it is organization which standarized different technologies
+// they are responsible for suppoted languages for browser they decide which support or not
+// they standarized js and make name to ecmaScript because javaScript was petend by Oracle
+
+console.log("-------------------------------- 1997 ---------------------------------------");
+// The ES1 was come
+// they decide all browser ued this
+// ES2
+// ES3
+// ES4
+
+
+console.log("-------------------------------- 2009 ---------------------------------------");
+// ES5 comes with many feature thats why many people use js
+
+console.log("-------------------------------- 2015 ---------------------------------------");
+// ES6 Biggest update in history of js
+
+// The community of ecma named as TC39 (technical community 39) thats team member was decided what we change in technology
+// they decide to give 6 meetings in year and decide what we added in js or not
+
+console.log("-------------------------------- 2016 ---------------------------------------");
+// ES7 : 2016 
+
+console.log("-------------------------------- 2017 ---------------------------------------");
+// ES8 : 2017 
+
+
+
+
+
+console.log("*******************************************************************************");
+// js is backword compatable means it propely work on old feature
+// js is not forword compatable 
+// EX : today i have add my feature in js 1 user used 2022's browser it cant work
+
+
+// with use of babel thats why it support in old browsers
