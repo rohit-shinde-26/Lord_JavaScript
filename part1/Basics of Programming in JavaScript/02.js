bcfc15bd17cc76a83c95a5c149fc933c669ad408
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-02-03 19:08:25 +0530

    here the html and02 js file

---
+++ part1/Basics of Programming in JavaScript/02.js
@@ -0,0 +1,21 @@
+"use strict";
+// intro to variables
+
+// variables can store some information
+// we can use that information later
+// we can change that information later
+
+// declare a variable 
+// var is a keyword defined by js
+var firstName = "Rohit";
+
+// use a variable 
+console.log(firstName);
+
+// change value 
+
+
+// variable names case sensitive 
+firstName = "Rudra";
+
+console.log(firstName);
