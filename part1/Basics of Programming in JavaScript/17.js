e902243d44a178ccc2402eb03eafd251f9978bc1
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-02-09 20:02:59 +0530

    17th file has added

---
+++ part1/Basics of Programming in JavaScript/17.js
@@ -0,0 +1,23 @@
+// nested if else
+
+// winning number 19 
+
+// 19 your guess is right 
+// 17 too low 
+// 20 too high 
+
+let winningNumber = 19;
+let userGuess = +prompt("Guess a number");  // used to accept input from user
+
+if(userGuess === winningNumber){
+    console.log("Your guess is right!!");
+}else{
+    if(userGuess < winningNumber){
+        console.log("too low !!!");
+    }else{
+        console.log("too high !!!");
+    }
+}
+
+
+
