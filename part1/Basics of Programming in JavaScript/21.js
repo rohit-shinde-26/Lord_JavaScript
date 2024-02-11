e86c2d9b6c188219fbfbf8cc4da8992de291aeea
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-02-11 20:43:01 +0530

    while loop example

---
+++ part1/Basics of Programming in JavaScript/21.js
@@ -0,0 +1,22 @@
+// while loop example 
+// 1+2+3+4+5+6+7+8+9
+let num = 100;
+let total = 0; //1 + 2 +3
+let i = 0;
+
+// logic
+// total = total +1
+// total = total +2
+// total = total +3
+// total = total +4
+
+while(i<=100){
+    total = total + i;
+    i++;
+}
+
+
+console.log(total);
+
+// let total = (num*(num+1))/2;  // mathematical formula
+// console.log(total);
