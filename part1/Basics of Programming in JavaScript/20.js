4c007da2b01b5586af269cd9029d63808e57e736
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-02-11 20:34:36 +0530

    while loop

---
+++ part1/Basics of Programming in JavaScript/20.js
@@ -0,0 +1,12 @@
+// while loop 
+
+// 0 se 9 
+// DRY => don't repeat yourself
+let i = 0; // 1 2 3 4
+
+while(i<=9){
+    console.log(i);
+    i++;
+}
+console.log(`current value of i is ${i}`);  //  string template
+console.log("hello");
