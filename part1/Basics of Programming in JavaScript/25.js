5bb4ed166eda381135c58c52b4e67d5a8bf1e98a
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-02-15 19:44:48 +0530

    do while loop

---
+++ part1/Basics of Programming in JavaScript/25.js
@@ -0,0 +1,15 @@
+// do while loop
+// let i = 0;
+// while(i<=9){
+//     console.log(i);
+//     i++;
+// }
+
+let i = 10;
+do{
+    console.log(i);
+    i++;
+    // i prints then exit loop
+}while(i<=9);
+
+console.log("value of i is ", i);  // i++ prints here
