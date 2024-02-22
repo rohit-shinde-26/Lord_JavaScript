a61a5685a5de22d6e418da5c47641c6f8e9f1a30
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-02-22 19:57:04 +0530

    computed properties

---
+++ part1/Objects in js/39.js
@@ -0,0 +1,20 @@
+// computed properties
+
+const key1 = "Name";
+const key2 = "Age";
+
+const val1 = "ram";
+const val2 = 20;
+
+// make object with key value pair 
+
+// const obj = {
+//     [key1] : val1,
+//     [key2] : val2
+
+// }
+const obj = {};
+obj[key1] = val1;
+obj[key2] = val2;
+
+console.log(obj)
