82465342829a17d1e6765000499f98902512eae7
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-02-28 19:48:04 +0530

    function expression

---
+++ part1/Functions in JS/45.js
@@ -0,0 +1,32 @@
+// function expression 
+// function singHappyBirthday(){
+//     console.log("happy birthday to you ......");
+// }
+
+const singHappyBirthday = function(){
+    console.log("happy birthday to you ......");
+}
+
+// singHappyBirthday();
+
+const sumThreeNumbers = function(number1, number2, number3){
+    return number1 + number2 + number3;
+}
+const ans = sumThreeNumbers(2,3,4);
+console.log(ans);
+
+
+// function isEven(number){
+//     return number % 2 === 0;
+// }
+const isEven = function(number){
+    return number % 2 === 0;
+}
+// console.log(isEven(2));
+
+const firstChar = function(anyString){
+    return anyString[0];
+}
+
+
+
