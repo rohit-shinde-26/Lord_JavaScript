213f7c2639edd6893d597456c689230bb12b24a3
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-03 19:18:09 +0530

    hoisting intro

---
+++ part1/Functions in JS/47.js
@@ -0,0 +1,20 @@
+// hoisting 
+// it used in function declaration
+hello();
+
+function hello(){
+    console.log("Jay Shree Ram");
+}
+
+// it cant used in function expression
+// name("Ram")
+// const name = (myName) => {
+//     console.log(myName)
+// }
+
+
+
+// var cant show error
+// console.log(hello);
+// const hello = "hello world";
+// console.log(hello);
