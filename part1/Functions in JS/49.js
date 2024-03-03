4bd4fff6580529e1d4f7e8b2e338ed673411626a
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-03 19:18:56 +0530

    lexical scope

---
+++ part1/Functions in JS/49.js
@@ -0,0 +1,22 @@
+// lexical scope 
+//  check the value step by step
+// myApp => myFunc => myFunc2
+const myVar = "value1";
+
+function myApp(){
+    
+
+    function myFunc(){
+        // const myVar = "value59";
+        const myFunc2 = () =>{
+            console.log("inside myFunc", myVar);
+        }
+        myFunc2();
+    }
+
+
+    console.log(myVar);
+    myFunc();
+}
+
+myApp();
