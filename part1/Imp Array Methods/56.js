6520ba14a7b4c86bad28417957b83457c58cf7b7
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-18 22:09:38 +0530

    method added

--- part1/Imp Array Methods/56.js
+++ part1/Imp Array Methods/56.js
@@ -15,7 +15,7 @@
 // })
 
 const users = [
-    {firstName: "harshit", age: 23},
+    {firstName: "Rohit", age: 23},
     {firstName: "mohit", age: 21},
     {firstName: "nitish", age: 22},
     {firstName: "garima", age: 20},
