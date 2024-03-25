44995c2da55026e9a54ad0de2e9fd1c8c42f274d
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-03-25 19:25:40 +0530

    find method

--- part1/Imp Array Methods/61.js
+++ part1/Imp Array Methods/61.js
@@ -1,21 +1,27 @@
 // find method 
 
-// const myArray = ["Hello", "catt", "dog", "lion"];
+const arr = ["Ram","Sham","Hari","Maruti"]
 
-// function isLength3(string){
-//     return string.length === 3;
-// }
+// const ans = arr.find((str)=>{
+//     return str.length===3
+// })
+// console.log(ans)
 
-// const ans = myArray.find((string)=>string.length===3);
-// console.log(ans);
 
 const users = [
-    {userId : 1, userName: "harshit"},
-    {userId : 2, userName: "harsh"},
-    {userId : 3, userName: "nitish"},
-    {userId : 4, userName: "mohit"},
-    {userId : 5, userName: "aaditya"},
-];
+    {firstName: "Rohit", age: 23},
+    {firstName: "mohit", age: 21},
+    {firstName: "nitish", age: 22},
+    {firstName: "garima", age: 20},
+]
+const ans = users.find((user)=> user.age>21)
+console.log(ans)
 
-const myUser = users.find((user)=>user.userId===3);
-console.log(myUser);
+
+
+
+
+
+
+
+
