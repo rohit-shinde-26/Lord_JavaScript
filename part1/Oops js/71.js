03b8456fc3ab993a22d01af6dbce8b6057851919
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-04-03 13:41:28 +0530

    methods.this

---
+++ part1/Oops js/71.js
@@ -0,0 +1,28 @@
+// methods
+// function inside object called method
+
+function personInfo(){
+    console.log(`person name is ${this.firstName} and age is ${this.age}`);
+}
+
+const person1 = {
+    firstName : "Rohit",
+    age: 8,
+    about: personInfo
+}
+const person2 = {
+    firstName : "mohit",
+    age: 18,
+    about: personInfo
+}
+const person3 = {
+    firstName : "nitish",
+    age: 17,
+    about: personInfo
+}
+
+person1.about();
+person2.about();
+person3.about();
+
+// this stands for object
