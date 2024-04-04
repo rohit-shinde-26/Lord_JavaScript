3771f8cd41bcd8ee43857d19fda5843948d5beb2
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-04-04 12:05:33 +0530

    update example

--- part1/Oops js/78.js
+++ part1/Oops js/78.js
@@ -18,8 +18,8 @@ function createUser(firstName, lastName, email, age, address){
     return user;
 }
 
-const user1 = createUser('harshit', 'vashsith', 'harshit@gmail.com', 9, "my address");
-const user2 = createUser('harsh', 'vashsith', 'harshit@gmail.com', 19, "my address");
-const user3 = createUser('mohit', 'vashsitha', 'harshit@gmail.com', 17, "my address");
+const user1 = createUser('Rohit', 'Shinde', 'rohit@gmail.com', 9, "my address");
+const user2 = createUser('Ram', 'Gunjal', 'ram@gmail.com', 19, "my address");
+const user3 = createUser('Akshay', 'kumar', 'akshay@gmail.com', 17, "my address");
 console.log(user1.about());
 console.log(user3.about());
