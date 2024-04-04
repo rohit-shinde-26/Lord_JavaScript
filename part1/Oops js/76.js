3ab0215e67a245ceca27fae4b5eca22cccfe9b82
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-04-04 10:33:38 +0530

    short syntax for func

---
+++ part1/Oops js/76.js
@@ -0,0 +1,19 @@
+// const user1 = {
+//     firstName : "Rohit",
+//     age: 8,
+//     about: function(){
+//         console.log(this.firstName, this.age);
+//     }   
+// }
+
+const user1 = {
+    firstName : "Abhi",
+    age: 18,
+    about(){
+        console.log(this.firstName, this.age);
+    }   
+}
+
+
+
+user1.about();
