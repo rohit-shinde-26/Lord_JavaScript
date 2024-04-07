8fdc390408345f33ef95efbd326df9740860fb3e
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-04-07 09:30:52 +0530

    prototype

---
+++ part1/Oops js/81.js
@@ -0,0 +1,34 @@
+function hello(){
+    console.log("hello world");
+}
+
+// javascript function ===> function  + object
+
+// console.log(hello.name);
+
+// you can add your own properties 
+
+
+// hello.myOwnProperty = "very unique value";
+// console.log(hello.myOwnProperty);
+
+
+// name property ---> tells function name;
+
+// function provides more usefull properties.
+
+
+// console.log(hello.prototype); // {}
+
+// only functions provide prototype property
+
+hello.prototype.age = 21
+console.log(hello.prototype.age)
+
+
+hello.prototype.abc = "abc";
+hello.prototype.xyz = "xyz";
+hello.prototype.sing = function(){
+    return "lalalla";
+};
+console.log(hello.prototype.sing());
