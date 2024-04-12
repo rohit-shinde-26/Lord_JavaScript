ee6c69c9f716231faa7756d4a3133cc68fc9c313
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-04-12 11:37:21 +0530

    global execution context

---
+++ part2/how-javascript-works/hjw_05.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+let firstName = "Rohit"; 
+
+const myFunction = function() {
+    let var1 = "First Variable";
+    let var2 = "second Variable";
+    console.log(var1);
+    console.log(var2);
+}
