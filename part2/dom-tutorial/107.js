8b2e092611cb7222bb8479013f994d9cb06b01b3
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-04-18 22:22:04 +0530

    get & set attributes

---
+++ part2/dom-tutorial/107.js
@@ -0,0 +1,8 @@
+// get and set attrubutes
+const link = document.querySelector("a");
+console.log(link.getAttribute("href").slice(1)); 
+// link.setAttribute("href", "https://rohit.com"); // change the value
+// console.log(link.getAttribute("href"));
+
+// const inputElement = document.querySelector(".form-todo input");
+// console.log(inputElement.getAttribute("type"));
